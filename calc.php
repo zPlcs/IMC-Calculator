67947286759e65318f2eab5a262a11af080cf5dd
Patrik Luiz <patrikluiz2004@hotmail.com>
2024-11-10 23:32:08 -0300

    Correção de alguns erros.

--- calc.php
+++ calc.php
@@ -1,31 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Calculadora de IMC</title>
-</head>
-<body>
-    <main>
-
-<h1>Calculadora de IMC</h1><br>
-    <form action="calc.php" method="post">
-        <label for="peso">Insira seu peso: </label>
-        <input type="number" name="peso" id="peso" required><br><br>
-
-        <label for="altura">Insira sua altura: </label>
-        <input type="number" step="0.01" name="altura" id="altura" required><br><br>
-
-        <input type="submit" value="Enviar">
-    </form>
-
 <?php
 
 $peso = $_POST['peso'];
 $altura = $_POST['altura'];
 
-if(isset($peso) && isset($altura)){
+if(isset($peso) && isset($altura) && $peso > 0 && $altura > 0){
     $imc = $peso/($altura*$altura);
     $formated_imc = number_format($imc,2, '.', '');
 
@@ -44,10 +22,36 @@ if(isset($peso) && isset($altura)){
     }
 
 } else {
-    echo '<br>Favor verificar os dados inseridos.';
+    $mensagem = "Insira os dados ou verifique se são validos.";
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculadora de IMC</title>
+</head>
+<body>
+    <main>
+
+<h1>Calculadora de IMC</h1><br>
+    <form action="calc.php" method="post">
+        <label for="peso">Insira seu peso: </label>
+        <input type="number" name="peso" id="peso" value="<?php echo $peso; ?>" min="0" required><br><br>
+
+        <label for="altura">Insira sua altura: </label>
+        <input type="number" step="0.01" name="altura" id="altura" value="<?php echo $altura; ?>" min="0" required><br><br>
+
+        <input type="submit" value="Enviar">
+    </form>
+
+    <img src="" alt="">
+
+    <p><?php echo $mensagem; ?></p>
 <hr>
 <h2>IMC: <?php echo $formated_imc; ?></h2>
 <h2>Classificação: <?php echo $imc_scale; ?></h2>
